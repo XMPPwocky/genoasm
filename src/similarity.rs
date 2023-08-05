01aa15ef1d3ad795f50982f80b0c1ee62a8e7f9c
XMPPwocky <mimir@xmppwocky.net>
2023-08-04 18:13:39 -0700

    uwu

--- src/similarity.rs
+++ src/similarity.rs
@@ -84,12 +84,12 @@ pub fn compare_spectrograms_internal<'a>(
                 let (l, r) = (l as f64, r as f64);
                 //let scale = 1.0;// f64::max(f64::max(l, r), 1e-10);
 
-                let diff = (l.ln() - r.ln()).abs();
+                let diff = ((l + 1e-40).ln() - (r + 1e-40).ln()).abs();
                 //println!("{}", diff);
                 if diff.is_finite() {
                     diff
                 } else {
-                    1e8
+                    1e20
                 }
             }).sum::<f64>()
         })
